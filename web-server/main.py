62fd768671dd08eb136812314bab036db05bc269
Germán Martínez <martinezdiazgerman@gmail.com>
2023-04-27 09:33:46 +0200

    Crear servidor con uvicorn y fastAPI

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,19 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,3]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_html():
+    return '''
+    <h1>Hola soy una pagina </h1>
+    <p> Y yo soy un párrafo</p>
+    '''
 
 def run():
     store.get_categories()
